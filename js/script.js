8c953d41ce649f138264364f14ba7e78c943f7ab
juanlove <juan.a.urena@gmail.com>
2016-03-14 17:45:18 -0700

    push

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
-$(function( ){
-   
-    $('#alertMe').click(function(e) {
-       
-        e.preventDefault();
-        
-        $('#successAlert').slideDown();
-        
-    });
-    
-    $('a.pop').click(function(e) {
-        
-        e.preventDefault();
-        
-    });
-    
-    $('a.pop').popover();
-    
-    $('[rel="tooltip"]').tooltip();
-    
-    
+$(function() {
+	
+	$('#alertMe').click(function(e) {
+		
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+		
+	});
+	
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+	
+	$('a.pop').popover();
+	
+	$('[rel="tooltip"]').tooltip();
+	
+	
 });
+
